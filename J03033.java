1dd5f37f40b5eae72948ba504154ef6f7a1e84b9
AnhN-V <94591826+AnhN-V@users.noreply.github.com>
2022-10-17 22:47:07 +0700

    Update J03033.java

--- J03033.java
+++ J03033.java
@@ -7,9 +7,9 @@ public class J03033 {
         BigInteger s = new BigInteger(a);
         BigInteger s1 = new BigInteger(b);
  
-        BigInteger mul = s.multiply(s1);
-        BigInteger gcd = s.gcd(s1);
-        BigInteger lcm = mul.divide(gcd);
+        BigInteger mul = s.multiply(s1);  // calculate multiplication of two bigintegers
+        BigInteger gcd = s.gcd(s1);  // calculate gcd of two bigintegers
+        BigInteger lcm = mul.divide(gcd);  // calculate lcm using formula: lcm * gcd = x * y
         return lcm;
     }
 
